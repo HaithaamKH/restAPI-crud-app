6b40fe3128eaa8e333824bb21b0bef7e4166a699
Haitham AL-Khateeb <146680973+HaithaamKH@users.noreply.github.com>
2024-10-25 18:46:16 +0300

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,10 +1,27 @@
-CREATE DATABASE IF NOT EXISTS userdb;
+-- Create the database
+CREATE DATABASE IF NOT EXISTS employee_directory;
 
-USE userdb;
+-- Use the created database
+USE employee_directory;
 
-CREATE TABLE IF NOT EXISTS student (
+-- Create the employee table
+CREATE TABLE IF NOT EXISTS employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    email VARCHAR(255)
+    first_name VARCHAR(45),
+    last_name VARCHAR(45),
+    email VARCHAR(45)
+);
+
+-- Create the authorities table
+CREATE TABLE IF NOT EXISTS authorities (
+    username VARCHAR(50),
+    authority VARCHAR(50),
+    PRIMARY KEY (username, authority) -- Composite primary key
+);
+
+-- Create the roles table
+CREATE TABLE IF NOT EXISTS roles (
+    user_id VARCHAR(50),
+    role VARCHAR(50),
+    PRIMARY KEY (user_id, role) -- Composite primary key
 );
